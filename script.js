ef9a9a02d0934b1d5fb84a47a16843b2e81fbfd4
Faizan Alam <technicalalam803@gmail.com>
2024-09-23 16:29:15 +0530

    first project commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('.main'),
+    smooth: true,
+    multiplier: 1.5,  // Controls the scroll speed
+    lerp: 0.1,  // Controls how smooth the scroll is
+    direction: 'vertical',  // Can also be 'horizontal'
+  });
+  
